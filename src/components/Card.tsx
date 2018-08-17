c0a7def320232c3bf29e4dfef22b7664664e1dd6
Test <karim_om@me.com>
2018-08-17 15:55:46 +0200

    Added all links to "presse-archiv" page, also added title tags to imgs and links.
    Signed-off-by: Test <karim_om@me.com>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import { CardMainDiv, CardImg, CardDiv, CardH4 } from './StyledComponents';
+import { CardMainDiv, CardImg, CardDiv, CardH4 } from './StyledComponents'
 
 class Card extends React.Component {
-  render() {
+  private render() {
     return (
       <CardMainDiv>
-        <CardImg alt={this.props.alt} src={this.props.img} />
+        <CardImg title={this.props.alt} alt={this.props.alt} src={this.props.img} />
         <CardDiv>
-          <a href={this.props.link}><CardH4>{this.props.name}</CardH4> </a>
+          <a title={this.props.name} target="blank_" href={this.props.link}><CardH4>{this.props.name}</CardH4> </a>
         </CardDiv>
       </CardMainDiv>
-    );
+    )
   }
 }
 export default Card
