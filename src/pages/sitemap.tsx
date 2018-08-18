06879e4b4d6f392b2280390de5cc65d42ef7e79a
Test <karim_om@me.com>
2018-08-18 18:06:37 +0200

    Changed Links in Sitemap to Gatsby-Link
    Signed-off-by: Test <karim_om@me.com>

--- src/pages/sitemap.tsx
+++ src/pages/sitemap.tsx
@@ -4,6 +4,7 @@ import Page from '../components/Page'
 import Container from '../components/Container'
 import styled from 'styled-components'
 import { colors } from '../styles/variables'
+import Link from 'gatsby-link'
 
 const Sitemap = () => (
   <Page>
@@ -20,61 +21,61 @@ const Sitemap = () => (
       <h1>Inhaltsverzeichnis</h1>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Startseite" href="/">
+        <LinkRed title="Link zu der Startseite" to="/">
           Home
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Pflegedienst" href="/pflegedienst">
+        <LinkRed title="Link zu der Pflegedienst" to="/pflegedienst">
           Pflegedienst
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Wohngemeinschaft" href="/wohngemeinschaften">
+        <LinkRed title="Link zu der Wohngemeinschaft" to="/wohngemeinschaften">
           Wohngemeinschaften
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Außerklinische Intensivpflege" href="/intensivpflege">
+        <LinkRed title="Link zu der Außerklinische Intensivpflege" to="/intensivpflege">
           Außerklinische Intensivpflege
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Leitbilder" href="/leitbilder">
+        <LinkRed title="Link zu der Leitbilder" to="/leitbilder">
           Leitbilder
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Presse | Archiv" href="/presse-archiv">
+        <LinkRed title="Link zu der Presse | Archiv" to="/presse-archiv">
           Presse | Archiv
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Team | Kontakt" href="/team-kontakt">
+        <LinkRed title="Link zu der Team | Kontakt" to="/team-kontakt">
           Team | Kontakt
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Jobs | Partner" href="/jobs">
+        <LinkRed title="Link zu der Jobs | Partner" to="/jobs">
           Jobs | Partner
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Engagement" href="/engagement">
+        <LinkRed title="Link zu der Engagement" to="/engagement">
           Engagement
         </LinkRed>
       </p>
       <p>
         >{' '}
-        <LinkRed title="Link zu der Impressum | Anfahrt" href="/impressum">
+        <LinkRed title="Link zu der Impressum | Anfahrt" to="/impressum">
           Impressum | Anfahrt
         </LinkRed>
       </p>
@@ -84,6 +85,6 @@ const Sitemap = () => (
 
 export default Sitemap
 
-export const LinkRed = styled.a`
+export const LinkRed = styled(Link)`
   color: ${colors.red};
 `
