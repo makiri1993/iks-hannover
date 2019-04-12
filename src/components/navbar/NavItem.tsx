597b01bca547e54c3c0ec74367b765fc9a1acb18
makiri <kireew.martin@icloud.com>
2019-04-12 09:52:12 +0200

    fix build

--- src/components/navbar/NavItem.tsx
+++ src/components/navbar/NavItem.tsx
@@ -3,7 +3,7 @@ import { NavItemProps } from './Nav'
 import { Link } from 'gatsby'
 
 interface NavItemState {
-  height: string;
+  height: string
 }
 
 export default class NavItem extends Component<NavItemProps, NavItemState> {
@@ -14,7 +14,7 @@ export default class NavItem extends Component<NavItemProps, NavItemState> {
     super(props)
     this.ref = createRef()
     this.state = {
-      height: '',
+      height: 'auto',
     }
   }
 
@@ -23,13 +23,7 @@ export default class NavItem extends Component<NavItemProps, NavItemState> {
     const { subItems } = this.props
     if (subItems) {
       const { offsetHeight } = this.ref.current
-      const {
-        marginTop,
-        marginBottom,
-        height,
-        paddingTop,
-        paddingBottom,
-      } = window.getComputedStyle(this.ref.current)
+      const { marginTop, marginBottom, height, paddingTop, paddingBottom } = window.getComputedStyle(this.ref.current)
       console.log(marginBottom, marginTop, height, paddingTop, paddingBottom)
       console.log(this.ref.current)
       console.log(this.ref.current.offsetHeight)
@@ -53,12 +47,7 @@ export default class NavItem extends Component<NavItemProps, NavItemState> {
     const { title, to, subItems } = this.props
     const { height } = this.state
     return (
-      <div
-        className='d-flex-column overflow-hidden height-transition'
-        style={{ height }}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-      >
+      <div className='d-flex-column overflow-hidden height-transition' style={{ height }} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
         {this.renderLinkOrNot({ title, to, ref: this.ref })}
         {subItems ? this.renderDropdown(subItems) : null}
       </div>
@@ -87,21 +76,13 @@ export default class NavItem extends Component<NavItemProps, NavItemState> {
     })
   }
 
-  private renderLinkOrNot({ title, to, ref, index }: { title: string, to?: string, ref?: RefObject<any>, index?: number }) {
+  private renderLinkOrNot({ title, to, ref, index }: { title: string; to?: string; ref?: RefObject<any>; index?: number }) {
     return to ? (
-      <Link
-        key={index}
-        ref={ref}
-        className='homepage-link hover-orange'
-        title={title}
-        to={to}
-      >
+      <Link key={index} ref={ref} className='homepage-link hover-orange' title={title} to={to}>
         {title}
       </Link>
     ) : (
-      <div
-        key={index}
-        className='homepage-link hover-orange' ref={ref}>
+      <div key={index} className='homepage-link hover-orange' ref={ref}>
         {title}
       </div>
     )
