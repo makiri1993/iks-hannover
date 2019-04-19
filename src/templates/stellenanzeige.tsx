db3d6d5cc04489747a63db12cc49a6275a635477
Karim Ould Mahieddine <karimouldmahieddine@karims-mbp.speedport_w_724v_09011603_05_017>
2019-04-19 20:55:29 +0200

    cms

---
+++ src/templates/stellenanzeige.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Container from '../components/Container'
+
+
+
+const Stellenanzeige: React.SFC = () => (
+  <Container>
+    <h1>JOB</h1>
+  </Container>
+)
+
+export default Stellenanzeige
