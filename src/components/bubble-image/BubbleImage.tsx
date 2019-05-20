b102cf964223b397d13f23f28dc37e74e8ef9b67
makiri <kireew.martin@icloud.com>
2019-05-20 21:39:10 +0200

    first rebuild of hero slider

---
+++ src/components/bubble-image/BubbleImage.tsx
@@ -0,0 +1,62 @@
+import Img, { FluidObject } from 'gatsby-image'
+import React, { Component } from 'react'
+
+interface State {
+  indexOfActiveImage: number
+  images: FluidObject[]
+}
+
+interface Props {
+  images: FluidObject[]
+}
+export default class BubbleImage extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    const { images } = this.props
+    this.state = {
+      indexOfActiveImage: 0,
+      images,
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.nextImage()
+    }, 3000)
+  }
+
+  private nextImage() {
+    const { images } = this.state
+    const image = images.pop()
+    if (image) {
+      images.unshift(image)
+    }
+    this.setState({ images })
+  }
+
+  private getTranslateX(multiplikator: number) {
+    return
+  }
+
+  render() {
+    const { images } = this.props
+    const { indexOfActiveImage } = this.state
+    return (
+      <div className='bubble-container'>
+        {images.map((image, index) => {
+          // const translateX: number = this.getTranslateX(index)
+
+          return (
+            <Img
+              key={index}
+              className='bubble'
+              style={{ transform: 'translateX(20%)', height: `${(index + 1) * 8}vh`, width: `${(index + 1) * 8}vh` }}
+              fluid={image}
+              alt='image in bubble form in container'
+            />
+          )
+        })}
+      </div>
+    )
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import EmployeeTile from '../components/EmployeeTile'
 import FourColumns from '../components/FourColumns'
 import Heading from '../components/heading/Heading'
-import HeroSlider from '../components/HeroSlider'
+import BubbleImage from '../components/bubble-image/BubbleImage'
 import Text from '../components/text/Text'
 import Helmet from 'react-helmet'
 
@@ -76,7 +76,7 @@ export default ({ data }: { data: IndexData }) => {
           },
         ]}
       />
-      <HeroSlider images={transformedHeroImages} />
+      <BubbleImage images={transformedHeroImages} />
       <div className='max-container'>
         <div className='text-container' style={{ paddingTop: '2.4rem' }}>
           <Heading size={1} uppercase center orange fontWeight={500}>
