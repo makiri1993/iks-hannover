9b1dc41ecc6d9c50e752ef95fcb46fbc2824b1f6
leonkireew <leon.kireew@gmail.com>
2022-09-12 16:30:16 +0200

    added yarn package manager, added prettier, removed default code at index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+
+const IndexPage = () => {
+  return <main></main>;
+};
+
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Home Page</title>;
