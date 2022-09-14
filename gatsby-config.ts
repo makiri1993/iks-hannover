0ff79850b98255f154be91c6d286e9231d3b5956
leonkireew <leon.kireew@gmail.com>
2022-09-14 03:08:14 +0200

    addded gatsby-source-graphql + generated cms at the localhost:80000/graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,21 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Remote schema query type. This is an arbitrary name.
+        typeName: "WPGraphQL",
+        // Field name under which it will be available. Used in your Gatsby query. This is also an arbitrary name.
+        fieldName: "cms",
+        // GraphQL endpoint, relative to your WordPress home URL.
+        url: "https://leuchturm.vita-list-hannover.de/graphql",
+      },
+    },
+  ],
 };
 
 export default config;
